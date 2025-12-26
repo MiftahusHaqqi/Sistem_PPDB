368f8e2b980d3e41514c5aae22ea5fd5a6c67c96
FauzanIlyasAlmeyda <ggid.uzen@gmail.com>
2025-12-26 17:56:57 +0700

    major admin

--- database/seeders/MajorSeeder.php
+++ database/seeders/MajorSeeder.php
@@ -8,11 +8,11 @@ use App\Models\Major;
 
 class MajorSeeder extends Seeder
 {
-    public function run()
-    {
-        // Gunakan create agar created_at terisi otomatis
-        Major::create(['nama_jurusan' => 'IPA', 'kuota' => 100]);
-        Major::create(['nama_jurusan' => 'IPS', 'kuota' => 100]);
-        Major::create(['nama_jurusan' => 'Bahasa', 'kuota' => 50]);
-    }
+    // public function run()
+    // {
+    //     // Gunakan create agar created_at terisi otomatis
+    //     Major::create(['nama_jurusan' => 'IPA', 'kuota' => 100]);
+    //     Major::create(['nama_jurusan' => 'IPS', 'kuota' => 100]);
+    //     Major::create(['nama_jurusan' => 'Bahasa', 'kuota' => 50]);
+    // }
 }
